604655fca217859742a922a70213992265541f62
sergiosgatidis <52936169+sergiosgatidis@users.noreply.github.com>
2026-08-20 22:40:11 -0700

    Initial commit: tube annotator PWA + test notebook

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Bump version string to force cache refresh after updating tube-annotator.html
+const CACHE = "lda-v1";
+const FILES = ["./tube-annotator.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  // Delete any old cache versions left over from previous installs
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
